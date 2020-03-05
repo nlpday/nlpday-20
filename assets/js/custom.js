e0a2510b463df014c14cc595a77742c081f14f8a
Łukasz Kobyliński <l.kobylinski@sages.com.pl>
2020-03-05 16:39:26 +0100

    Zmiany 2020

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,7 +57,7 @@ jQuery(function ($) {
 	/* ==========================================================================
    countdown timer
    ========================================================================== */
- jQuery('#clock').countdown('2020/06/19 08:00:00',function(event){
+ jQuery('#clock').countdown('2020/06/23 08:00:00',function(event){
   var $this=jQuery(this).html(event.strftime(''
   +'<div class="time-entry days"><span>%-D</span> Days</div> '
   +'<div class="time-entry hours"><span>%H</span> Hours</div> '
